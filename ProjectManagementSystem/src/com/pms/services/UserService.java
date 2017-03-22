d4c40fd451d3a1cc86101fa7d347af62d14c46cc
Brian <Brian>
2017-03-22 14:11:38 +0630

    removed folder

--- ProjectManagementSystem/src/com/pms/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.pms.services;
-
-import java.util.List;
-import com.pms.form.User;
-
-public interface UserService {
-	public List<User> getAllUsers();
-	public User getUser(int userId);
-}
